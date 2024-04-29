6a1f18749aab56d86e8f4fa15dacd065117fe028
mdrihan34 <mdrihan0019@gmail.com>
2024-04-30 01:47:04 +0600

    delete method is add

--- index.js
+++ index.js
@@ -1,19 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+// 0tfn194fTRDZl4SF
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 5000;
 
-const express = require('express')
-const app = express()
+const uri = `mongodb+srv://tourism-server:0tfn194fTRDZl4SF@cluster0.xojyj43.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const spotCollection = client.db("spotDB").collection("spot");
+    app.get('/spots', async(req, res)=>{
+      const cursor = spotCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+    
+    app.post('/spots', async(req, res)=>{
+     const newSpots = req.body
+      console.log(newSpots)
+      const result = await spotCollection.insertOne(newSpots)
+      res.send(result)
+    })
+    app.delete('/spots/:id', async(req, res) =>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await spotCollection.deleteOne(query)
+      res.send(result)
+    })
 
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
-app.use(cors())
-app.use(express.json())
-const port = process.env.PORT || 5000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+app.get("/", (req, res) => {
+  res.send("Hello spots world!");
+});
 
 app.listen(port, () => {
-  console.log(`Tourists Server is runing on port ${port}`)
-})
+  console.log(`Tourists Server is runing on port ${port}`);
+});
+run().catch(console.error);
